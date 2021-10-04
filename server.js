5e52459f790709d990875d7484a0074f739b5646
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-10-04 16:58:52 -0600

    added get requests

--- server.js
+++ server.js
@@ -76,6 +76,18 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+  });
+
+  app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  });
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
 app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
